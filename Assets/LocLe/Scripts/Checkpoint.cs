70db47d43ef2c2a584928f89d3dcc7147f84d000
L0cL3 <58381284+LocVTLe@users.noreply.github.com>
2021-04-22 11:29:14 -0500

    Update
    Rearrange/Rescale Enemies & Speedpads
    Create Foreground, Farforground
    Create Checkpoint with Dialog

---
+++ Assets/LocLe/Scripts/Checkpoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Collider2D player = null;
+    public GameObject spawnPoint = null;
+    public GameObject dialog = null;
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (player != null && other == player)
+        {
+            if (spawnPoint != null) spawnPoint.transform.position = this.transform.position;
+            if (dialog != null) dialog.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (player != null && other == player)
+        {
+            //if (spawnPoint != null) spawnPoint.transform.position = this.transform.position;
+            if (dialog != null) dialog.SetActive(false);
+        }
+    }
+
+}
